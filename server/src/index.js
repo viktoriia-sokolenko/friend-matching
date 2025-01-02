4dadeb23ee60fb59fe35555e27dc865e57fe0207
Your Name <viktoriia.i.sokolenko@gmail.com>
2025-01-02 23:06:47 +0200

    added getUserById

--- server/src/index.js
+++ server/src/index.js
@@ -27,6 +27,22 @@ app.get('/users', async (req, res) => {
   }
 });
 
+app.get('/users/:id', async (req, res) => {
+    const { id } = req.params;
+    console.log(`Received request for user with id: ${id}`);
+    try {
+      const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+  
+      if (result.rows.length === 0) {
+        return res.status(404).send('User not found');
+      }
+      res.json(result.rows[0]);
+    } catch (error) {
+      console.error('Error fetching user:', error);
+      res.status(500).send('Server Error');
+    }
+  });
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
