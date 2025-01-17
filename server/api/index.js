e6e38de0d357962b8d5afd3f61df4c863ae56572
Your Name <viktoriia.i.sokolenko@gmail.com>
2025-01-17 16:44:16 -0600

    deployment fixes

--- server/api/index.js
+++ server/api/index.js
@@ -20,15 +20,6 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
 }));
-app.options('*', (req, res, next) => {
-    const origin = req.get('Origin');
-    if (allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', origin);
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    }
-    res.status(200).end();
-});
 
 app.use(express.json());
 
