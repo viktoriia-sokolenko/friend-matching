c27a746aedbed6de8feb2471077b019835034c31
Your Name <viktoriia.i.sokolenko@gmail.com>
2025-01-17 16:19:00 -0600

    deployment fixes

--- server/api/index.js
+++ server/api/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3001;
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 const cors = require('cors');
-const allowedOrigins = ['http://localhost:3000', 'https://friend-matching-pb7uwlc3h-viktoriia-sokolenkos-projects.vercel.app'];
+const allowedOrigins = ['http://localhost:3000', 'https://friend-matching-lyart.vercel.app'];
 app.use(cors({
     origin: (origin, callback) => {
         if (!origin || allowedOrigins.includes(origin)) {
